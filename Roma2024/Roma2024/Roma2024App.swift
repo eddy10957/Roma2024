208a049999c68d2dd4c8d50e45467696473809fc
eddy10957 <eddytd96@gmail.com>
2023-05-05 11:02:48 +0200

    First Commit

---
+++ Roma2024/Roma2024/Roma2024App.swift
@@ -0,0 +1,17 @@
+//
+//  Roma2024App.swift
+//  Roma2024
+//
+//  Created by Edoardo Troianiello on 05/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Roma2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
