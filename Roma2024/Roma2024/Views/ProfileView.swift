c67c5f11c499b5417435af1f0f931721de891af3
Andrea Masturzo <92364690+AndreaMasturzo@users.noreply.github.com>
2023-05-08 22:25:51 +0200

    ProfileView Refactoring

--- Roma2024/Roma2024/Views/ProfileView.swift
+++ Roma2024/Roma2024/Views/ProfileView.swift
@@ -9,123 +9,152 @@ import SwiftUI
 
 struct ProfileView: View {
     @Environment(\.presentationMode) var presentation
-    @EnvironmentObject var viewModel : ViewModel
+    @EnvironmentObject var viewModel: ViewModel
     
     @State var notificationToggle : Bool = false
     
     var body: some View {
-        NavigationView{
-            VStack(alignment: .leading){
-                HStack{
-                    Image(viewModel.profile.image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(Circle())
-                    .frame(width: 150)
-                    .padding()
-                        
-                    VStack(alignment: .leading){
-                        Text(viewModel.profile.name)
-                        .font(.title)
-                        Text(viewModel.profile.surname)
-                        .font(.title2)
-            
-                    }
-                }
-                HStack{
-                    Text("Favorites")
-                        .font(.title)
-                    Spacer()
-                    Button {
-                        print("more Clicked")
-                    } label: {
-                        Text("more")
-                    }
-
-                }
-                Divider()
-                
-                
-                
-                VStack(alignment: .leading){
-                    //MARK: Nations -
-                    Text("Nations")
-                        .font(.title2)
-                    ScrollView(.horizontal){
-                        HStack(spacing: 5){
-                            ForEach(viewModel.favoritesCountries, id: \.self) { country in
-                                Image(country.rawValue.lowercased())
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: 50,height: 50)
-                            }
-                        }
-                    }
-                    
-                    //MARK: Athletes -
-                    Text("Athletes")
-                        .font(.title2)
-                    ScrollView(.horizontal){
-                        HStack(spacing: 5){
-                            ForEach(viewModel.athletes.filter({
-                                $0.isFavorite
-                            }), id: \.self) { athlete in
-                                Image(athlete.image)
-                                    .resizable()
-                                    .scaledToFill()
-                                    .clipShape(Circle())
-                                    .frame(width: 50,height: 50)
-                            }
-                        }
-                    }
-                    
-                    //MARK: Disciplines -
-                    Text("Disciplines")
-                        .font(.title2)
-                    ScrollView(.horizontal){
-                        HStack(spacing: 5){
-                            ForEach(viewModel.favoritesDisciplines, id: \.self) { discipline in
-                                ZStack{
-                                    Image(discipline.rawValue.lowercased())
-                                        .resizable()
-                                        .scaledToFill()
-                                        .frame(width: 40,height: 40)
-                                        
-                                    
-                                    Circle()
-                                        .stroke(lineWidth: 2)
-                                        .frame(width: 50,height: 50)
-
-                                        
-                                }
-                                .foregroundColor(Color.primaryDetail)
-
-                            }
-                        }
-                    }
-                }
-                
-                Text("Settings")
+        VStack(alignment: .leading) {
+            profileHeader
+            HStack {
+                Text("Favorites")
                     .font(.title)
-                
-                Divider()
-                
-                Toggle("Notifications", isOn: $notificationToggle)
-                
+                Spacer()
+                Button(action: {
+                    print("more Clicked")
+                }, label: {
+                    Text("more")
+                })
+                .tint(.hyperAccent)
             }
-            .padding()
-            .toolbar(content: {
-                ToolbarItem(placement: .navigationBarTrailing){
-                    Image(systemName: "chevron.down")
-                        .foregroundColor(Color.primaryDetail)
-                        .onTapGesture {
-                            presentation.wrappedValue.dismiss()
-                        }
+            .padding(.horizontal)
+            
+            Divider()
+            
+            
+            
+            VStack(alignment: .leading) {
+                nations
+                
+                athletes
+                
+                discipline
+            }
+            
+            Text("Settings")
+                .font(.title)
+            
+            Divider()
+            
+            Toggle("Notifications", isOn: $notificationToggle)
+            Spacer()
+            
+        }
+        .overlay(alignment: .topTrailing) {
+            Image(systemName: "chevron.down")
+                .foregroundColor(Color.primaryDetail)
+                .onTapGesture {
+                    presentation.wrappedValue.dismiss()
                 }
-            })
-           
+                .padding()
         }
+    }
+    private var profileHeader: some View {
+        return HStack {
+            Image(viewModel.profile.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(Circle())
+                .frame(height: 120)
+            
+            VStack(alignment: .leading){
+                Text(viewModel.profile.name)
+                    .font(.title)
+                Text(viewModel.profile.surname)
+                    .font(.title2)
+            }
         }
+        .padding()
+    }
+    
+    private var nations: some View {
+        return Group {
+            Text("Nations")
+                .font(.title2)
+            ScrollView(.horizontal) {
+                HStack(spacing: 5) {
+                    ForEach(viewModel.favoritesCountries, id: \.self) { country in
+                        FavoritesElement(favoriteType: .nation, imageName: country.rawValue)
+                    }
+                }
+            }
+        }
+    }
+    
+    private var athletes: some View {
+        return Group {
+            Text("Athletes")
+                .font(.title2)
+            ScrollView(.horizontal){
+                HStack(spacing: 5){
+                    ForEach(viewModel.athletes.filter({
+                        $0.isFavorite
+                    }), id: \.self) { athlete in
+                        FavoritesElement(favoriteType: .athlete, imageName: athlete.image)
+                    }
+                }
+            }
+        }
+    }
+    
+    private var discipline: some View {
+        return Group {
+            Text("Disciplines")
+                .font(.title2)
+            ScrollView(.horizontal){
+                HStack(spacing: 5){
+                    ForEach(viewModel.favoritesDisciplines, id: \.self) { discipline in
+                        FavoritesElement(favoriteType: .discipline, imageName: discipline.rawValue)
+                    }
+                }
+            }
+        }
+    }
+}
+
+enum FavoriteType {
+    case nation, athlete, discipline
+}
+struct FavoritesElement: View {
+    var favoriteType: FavoriteType
+    var imageName: String
+    var body: some View {
+        if favoriteType == .nation {
+            Image(imageName.lowercased())
+                .resizable()
+                .scaledToFill()
+                .frame(width: 50,height: 50)
+        } else if favoriteType == .athlete {
+            Image(imageName)
+                .resizable()
+                .scaledToFill()
+                .clipShape(Circle())
+                .frame(width: 50,height: 50)
+        } else if favoriteType == .discipline {
+            ZStack {
+                Image(imageName.lowercased())
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40,height: 40)
+                
+                
+                Circle()
+                    .stroke(lineWidth: 2)
+                    .frame(width: 50,height: 50)
+            }
+            .foregroundColor(Color.primaryDetail)
+        }
+    }
 }
 
 //struct ProfileView_Previews: PreviewProvider {
