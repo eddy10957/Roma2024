c504ca79033caeaaa9d1f53236cc424ff1271ac4
Raffaele S <coverforhobbyit@gmail.com>
2023-05-09 11:37:18 +0200

    Added the Pin in the MapView

--- Roma2024/Roma2024/Views/MapView.swift
+++ Roma2024/Roma2024/Views/MapView.swift
@@ -15,7 +15,65 @@ struct MapView: View {
             Image("map2")
                 .resizable()
                 .scaledToFill()
+            
+            ZStack {
                 
+                // Competition Venue
+                Button {
+                    
+                } label: {
+                    MapPin(width: 30)
+                }
+                .offset(x: -85, y: -270)
+                
+                // Warm-Up Area
+                Button {
+                    
+                } label: {
+                    MapPin(width: 30)
+                }
+                .offset(x: 90, y: -310)
+                
+                // Court 1
+                Button {
+                    
+                } label: {
+                    MapPin(width: 30)
+                }
+                .offset(x: -30, y: -10)
+                
+                // Court 2
+                Button {
+                    
+                } label: {
+                    MapPin(width: 30)
+                }
+                .offset(x: -15, y: 75)
+                
+                // Court 3
+                Button {
+                    
+                } label: {
+                    MapPin(width: 30)
+                }
+                .offset(x: -3, y: 135)
+                
+                // Court 4
+                Button {
+                    
+                } label: {
+                    MapPin(width: 30)
+                }
+                .offset(x: 18, y: 225)
+                
+                // Court 5
+                Button {
+                    
+                } label: {
+                    MapPin(width: 30)
+                }
+                .offset(x: 30, y: 290)
+            }
             HStack{
                 ZStack(alignment: .center){
                     
@@ -60,5 +118,31 @@ struct MapView: View {
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
+    }
+}
+
+
+struct MapPin: View {
+    @State var width : CGFloat
+    var body: some View {
+        VStack(spacing: 0) {
+                    Image(systemName: "map.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: width, height: width)
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding(6)
+                        .background(Color.primaryBackground)
+                        .clipShape(Circle())
+                    
+                    Image(systemName: "triangle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(.primaryBackground)
+                        .frame(width: width/3, height: width/3)
+                        .rotationEffect(Angle(degrees: 180))
+                        .offset(y: -3)
+                }
     }
 }
